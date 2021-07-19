ea5ebdfb1533508d90b588faf2d120e4b2d5ce05
José Quinde <49071271+JoseQ1996@users.noreply.github.com>
2021-07-19 12:49:22 -0500

    5.Programando LLave

--- Assets/UdacityVR/Scripts/Key.cs
+++ Assets/UdacityVR/Scripts/Key.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Key : MonoBehaviour {
 
-	// TODO: Create variables to reference the game objects we need access to
-	// Declare a GameObject named 'keyPoofPrefab' and assign the 'KeyPoof' prefab to the field in Unity
-	// Declare a Door named 'door' and assign the top level 'Door' game object to the field in Unity
+    // TODO: Create variables to reference the game objects we need access to
+    // Declare a GameObject named 'keyPoofPrefab' and assign the 'KeyPoof' prefab to the field in Unity
+    public GameObject keyPoofPrefab;
+    // Declare a Door named 'door' and assign the top level 'Door' game object to the field in Unity
+    public Door door;
 
-
-	void Update () {
-		// OPTIONAL-CHALLENGE: Animate the key rotating
-		// TIP: You could use a method from the Transform class
-	}
+    void Update () {
+        // OPTIONAL-CHALLENGE: Animate the key rotating
+        // TIP: You could use a method from the Transform class
+        transform.Rotate(0.0f, 0.0f, 1.0f);
+    }
 
 
 	public void OnKeyClicked () {
@@ -25,9 +27,12 @@ public class Key : MonoBehaviour {
 		// Prints to the console when the method is called
 		Debug.Log ("'Key.OnKeyClicked()' was called");
 
-		// TODO: Unlock the door, display the poof effect, and remove the key from the scene
-		// Use 'door' to call the Door.Unlock() method
-		// Use Instantiate() to create a clone of the 'KeyPoof' prefab at this coin's position and with the 'KeyPoof' prefab's rotation
-		// Use Destroy() to delete the key after for example 0.5 seconds
-	}
+        // TODO: Unlock the door, display the poof effect, and remove the key from the scene
+        // Use 'door' to call the Door.Unlock() method
+        door.Unlock();
+        // Use Instantiate() to create a clone of the 'KeyPoof' prefab at this coin's position and with the 'KeyPoof' prefab's rotation
+        Object.Instantiate(keyPoofPrefab, this.transform.position, keyPoofPrefab.transform.rotation);
+        // Use Destroy() to delete the key after for example 0.5 seconds
+        Object.Destroy(this.gameObject, 0.5f);
+    }
 }
